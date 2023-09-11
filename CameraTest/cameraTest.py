72f1aec7f7b24d6c964859f9bae555a8071b695d
Laceleaf <amaliewilke@gmail.com>
2023-09-11 19:03:29 +0200

    Testing Camera Livestream in separate flask application

---
+++ CameraTest/cameraTest.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, Response
+import picamera
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def generate_frames():
+    with picamera.PiCamera() as camera:
+        camera.resolution=(640, 480)
+        camera.framerate=30
+
+        while True:
+            frame=bytearray()
+            camera.capture(frame, 'jpeg', use_video_port=True)
+            yield(b'--frame\r\n'b'content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipar/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    app.run(host='169.254.170.93', port=5000, debug=True)
