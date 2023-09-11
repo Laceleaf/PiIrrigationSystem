87d822df734fac158f5324278514f19a30f772e5
Laceleaf <amaliewilke@gmail.com>
2023-09-11 13:06:52 +0200

    Basic if slightly chaotic website that shows active plants in system on main page and when clicked on leads to graphs displaying their sensor values

--- web/irrigation_web.py
+++ web/irrigation_web.py
@@ -13,6 +13,9 @@ import sqlite3
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
+sqlite_select_WHERE="""SELECT * FROM sensordata WHERE plant_id= ?"""
+sqlite_select_WHERE_plantname="""SELECT plant_id FROM planttable WHERE plant_name= ?"""
+
 #get most current data from database irrigation_db
 def currentData():
 	conn=sqlite3.connect("/home/molly/irrigation_db")
@@ -63,17 +66,67 @@ def getAllData(num):
 		allLight.append(row[4])
 		
 	return allTime, allTemp, allSoil, allLight
+
+#get sensor data from database from specifi plant using their ID	
+def getPlantData(num, plantID):
+	#plantID=(plantID,)
+	conn=sqlite3.connect("/home/molly/irrigation_db")
+	curs=conn.cursor()
+	#maybe prettier way here to select how much values to display, currently last 20
+	curs.execute(sqlite_select_WHERE, plantID)
+	allData=curs.fetchmany(num)
+	#initialize empty arrays for flood of data
+	allTime=[]
+	allTemp=[]
+	allSoil=[]
+	allLight=[]
+	
+	conn.close()
+	
+	#reversed(): reverse of given sequence of data above, returned as list
+	for row in reversed(allData):
+		allTime.append(row[5])
+		allTemp.append(row[3])
+		allSoil.append(row[2])
+		allLight.append(row[4])
 		
-		
+	return allTime, allTemp, allSoil, allLight
+
+#Dictionary that maps plant types from database (ID 1..) to icons of their type
+plant_type_images={
+	'1': 'tomato.png',
+	'2': 'basil.png'
+	}
+	
+#retrieves all plants from planttable to display		
+def get_plants():
+	conn=sqlite3.connect("/home/molly/irrigation_db")
+	curs=conn.cursor()
+	curs.execute("SELECT plant_name, plant_type, plant_id FROM planttable ORDER BY plant_id ASC")
+	plants=curs.fetchall()
+	
+	conn.close()
+	
+	for row in plants:
+		print(row[0])
+
+	return plants
+	
+	
 @app.route('/')
 def index(): #index view function to return result of calling render_template with index.html
 	measured, temp, light, soil=currentData()
+	plants=get_plants()
 	htmlData={
 	'time':measured,
 	'temp':temp,
 	'light':light,
-	'soil': soil
+	'soil': soil,
+	'plants': plants,
+	'plant_type_images': plant_type_images
 	}
+	
+
 	return render_template('indexBoot.html', **htmlData) #looks for index.html folder in templates folder
 	
 @app.route('/about')
@@ -102,7 +155,111 @@ def plot_soil():
 	response.mimetype='image/png'
 	return response	
 
+@app.route('/plot', methods=['Get', 'POST'])
+def plot_values(): 
+	plantID_str=request.form.get('plantID')
 	
+	#if plantID_str and plantID_str.isdigit():
+	
+	#plantID=int(plantID_str)
+	limit=20
+	#times, temps, soils, lights=getAllData(30) #getPlantData(limit, plant_id)
+	times,temps, soils, lights=getPlantData(20, (1,))
+	yaxis=soils
+	fig=Figure()
+	axis=fig.add_subplot(1, 1, 1)
+	axis.set_title("Bodenfeuchte")
+	axis.set_xlabel("Uhrzeit")
+	axis.grid(True)
+	xs=times
+	axis.plot(xs, yaxis)
+	
+			#Rotate the x-axis labels by 45 degrees
+	axis.set_xticklabels(xs, rotation=45)
+	
+	canvas=FigureCanvas(fig)
+	output=io.BytesIO()
+	canvas.print_png(output)
+	response=make_response(output.getvalue())
+	response.mimetype='image/png'
+	return response	
+		
+	#return "Invalid or missing Plant ID"
+	
+@app.route('/plot/light', methods=['Get', 'POST'])
+def plot_light(): 
+	plantID_str=request.form.get('plantID')
+	
+	#if plantID_str and plantID_str.isdigit():
+	
+	#plantID=int(plantID_str)
+	limit=20
+	#times, temps, soils, lights=getAllData(30) #getPlantData(limit, plant_id)
+	times,temps, soils, lights=getPlantData(20, (1,))
+	yaxis=lights
+	fig=Figure()
+	axis=fig.add_subplot(1, 1, 1)
+	axis.set_title("Licht")
+	axis.set_xlabel("Uhrzeit")
+	axis.grid(True)
+	xs=times
+	axis.plot(xs, yaxis)
+	
+			#Rotate the x-axis labels by 45 degrees
+	axis.set_xticklabels(xs, rotation=45)
+	
+	canvas=FigureCanvas(fig)
+	output=io.BytesIO()
+	canvas.print_png(output)
+	response=make_response(output.getvalue())
+	response.mimetype='image/png'
+	return response	
+		
+	#return "Invalid or missing Plant ID"
+	
+@app.route('/plot/temp', methods=['Get', 'POST'])
+def plot_temp(): 
+	plantID_str=request.form.get('plantID')
+	
+	#if plantID_str and plantID_str.isdigit():
+	
+	#plantID=int(plantID_str)
+	limit=20
+	#times, temps, soils, lights=getAllData(30) #getPlantData(limit, plant_id)
+	times,temps, soils, lights=getPlantData(20, (1,))
+	yaxis=temps
+	fig=Figure()
+	axis=fig.add_subplot(1, 1, 1)
+	axis.set_title("Temperatur")
+	axis.set_xlabel("Uhrzeit")
+	axis.grid(True)
+	xs=times
+	axis.plot(xs, yaxis)
+	
+			#Rotate the x-axis labels by 45 degrees
+	axis.set_xticklabels(xs, rotation=45)
+	
+	canvas=FigureCanvas(fig)
+	output=io.BytesIO()
+	canvas.print_png(output)
+	response=make_response(output.getvalue())
+	response.mimetype='image/png'
+	return response	
+		
+	#return "Invalid or missing Plant ID"
+		
+	
+@app.route('/plant/<string:plant_name>')
+def plant_detail(plant_name):
+	conn=sqlite3.connect("/home/molly/irrigation_db")
+	curs=conn.cursor()
+	curs.execute(sqlite_select_WHERE_plantname, (plant_name,))
+	plantID=curs.fetchone()
+	getPlantData(20, plantID)
+	
+	#html
+	
+	return render_template('plant.html', plant_name=plant_name, plant_id=plantID)
 
 if __name__=='__main__':
 	app.run(host='169.254.170.93', port=5000, debug=True)
